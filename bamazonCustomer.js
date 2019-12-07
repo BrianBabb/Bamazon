d204f8e13e8548f50d8ea55a07cb38a6c407d129
brian babb <babb.brian@gmail.com>
2019-12-07 10:18:38 -0500

    update js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -21,7 +21,8 @@ connection.connect(function (err) {
   start();
 });
 
-
+var cost = 0;
+var quantity = 0;
 //show inventory first after the connection is made
 function start() {
   connection.query("SELECT item_id, product_name, price FROM products", function (err, res) {
@@ -39,9 +40,63 @@ function start() {
         type: "input",
         message: "How many of item_id would you like?"
       }
-    ]) 
-    
+    ]).then(function (stock) {
+      connection.query("SELECT * FROM products WHERE ?",
+        { item_id: stock.choice }, function (err, res) {
+          if (err) throw err;
+
+          if (stock.quantity > res[0].stock_quantity) {
+            inquirer.prompt([
+              {
+                name: "continue",
+                type: "input",
+                message: "Sorry, out of stock of that item",
+
+              }
+
+            ]).then(function (stock) {
+              if (stock.continue == 'yes') {
+                start()
+
+              } else if (cost >= 0) {
+                console.log("total amount due is", parseFloat(Math.round(cost * 100) / 100).toFixed(2));
+                connection.end();
+              } else {
+                console.log("Thank you, Come again!");
+              }
+
+            });
+          } else {
+            console.log("fully stocked");
+            totalCost = cost + res[0].price * stock.quantity;
+            var quantity = res[0].stock_quantity - stock.quantity;
+            connection.query(`UPDATE products SET stock_quantity=${quantity} WHERE item_id = ${stock.choice}`, function (res) {
+              inquirer
+                .prompt([
+
+                  {
+                    type: "input",
+                    message: "Place another order? ",
+                    name: "continue"
+                  }
+                ]).then(function (stock) {
+                  if (stock.continue == "yes") {
+                    start()
+                  } else {
+                    console.log("your, total is  $", totalCost);
+                    connection.end();
+                  }
+
+                })
+            })
+
+          };
+
+        })
+
+    })
+
   });
-  
+
 
 }
